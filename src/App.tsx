a4619e4fbaaaeac0cc403ea3240cd086387d3458
Garraju Naralasetti (Quadrant Technologies) <garraju.naralasetti@quadranttechnologies.com>
2026-04-17 16:41:09 +0530

    Updated App.tsx with DataProvider and DataSources route

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { Toaster } from "@/components/ui/toaster";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Forecasting from "./pages/Forecasting";
+// import POTracking from "./pages/POTracking";
+// import DashboardLayout from "./components/DashboardLayout";
+// import NotFound from "./pages/NotFound";
+
+// const queryClient = new QueryClient();
+
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <TooltipProvider>
+//       <Toaster />
+//       <Sonner />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Login />} />
+//           <Route element={<DashboardLayout />}>
+//             <Route path="/dashboard" element={<Dashboard />} />
+//             <Route path="/forecasting" element={<Forecasting />} />
+//             <Route path="/po-tracking" element={<POTracking />} />
+//           </Route>
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </TooltipProvider>
+//   </QueryClientProvider>
+// );
+
+// export default App;
+
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -9,27 +46,34 @@ import Forecasting from "./pages/Forecasting";
 import POTracking from "./pages/POTracking";
 import DashboardLayout from "./components/DashboardLayout";
 import NotFound from "./pages/NotFound";
+import DataSources from "./pages/DataSources";
+import { DataProvider } from "./context/DataContext";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/forecasting" element={<Forecasting />} />
-            <Route path="/po-tracking" element={<POTracking />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <DataProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route element={<DashboardLayout />}>
+                <Route path="/data-sources" element={<DataSources />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/forecasting" element={<Forecasting />} />
+                <Route path="/po-tracking" element={<POTracking />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </DataProvider>
+  );
+}
 
 export default App;
